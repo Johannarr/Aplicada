0e8d9a503fc28f84730a86b8171a3c3fe1308f90
Johanna Rodriguez <39653403+Johannarr@users.noreply.github.com>
2020-02-11 02:40:15 -0400

    Web api casi funciona
    Web api del profe

---
+++ Gravilota/Assets/Scripts/WebServiceClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebServiceClient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Serializable]
+    public class GravilotaScore
+    {
+        int Id;
+        string PlayerName;
+        double Score;
+    }
+    UnityWebRequest www;
+    const string webServiceURL = "https://localhost:44343/api/values";
+    void Start()
+    {
+
+      //StartCoroutine("SendWebRequest");
+        
+    }
+
+   public IEnumerator SendWebRequest(double score)
+    {
+       
+       GravilotaScore newScore = new GravilotaScore();
+       newScore.PlayerName = "Joha";
+       newScore.Score = score;
+
+       //www.UnityWebRequest.Post(webServiceURL, JsonUtility.ToJson(MI_OBJETO));
+        www = UnityWebRequest.Put(webServiceURL, JsonUtility.ToJson(newScore));
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        Debug.Log(www.downloadHandler.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
