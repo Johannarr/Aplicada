2e339305b6308d3500456b53a396630ebfb244a0
Johanna Rodriguez <39653403+Johannarr@users.noreply.github.com>
2020-03-26 19:51:00 -0400

    Taberna

---
+++ Gravilota/Assets/Scripts/Taberna/CameraMovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject _player;
+    Vector3 _newPosition;
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    void Start()
+    {
+        _newPosition = gameObject.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _newPosition.y=Mathf.Clamp(_player.transform.position.y, -2.25f, 2.25f);
+        transform.position=_newPosition;
+    }
+}
---
+++ Gravilota/Assets/Scripts/Taberna/GameLoaderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoaderController : MonoBehaviour
+{
+
+    string _sceneName;
+    public void OnTriggerStay (Collider other)
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            switch(gameObject.name)
+            {
+                case "GameLoader1":
+                _sceneName="FlappyBird";
+                break;
+             }
+            
+        }
+
+    SceneManager.LoadScene(_sceneName);
+    }
+    // Start is called before the first frame update
+}
