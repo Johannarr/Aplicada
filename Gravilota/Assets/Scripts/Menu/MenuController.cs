493e51776776acabc5eb1b0cd1c0cd09f57112be
Johanna Rodriguez <39653403+Johannarr@users.noreply.github.com>
2020-03-31 21:31:54 -0400

    menu

---
+++ Gravilota/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    GameObject _canvas;
+
+    private void Awake()
+    {
+        _canvas= GameObject.Find("GameOptionsDialog");
+        _canvas.SetActive(false);
+    }
+
+    public void ShowGameOptions()
+    {
+        _canvas.SetActive(true);
+    }
+
+    public void HidenGameOptions()
+    {
+        _canvas.SetActive(false);
+    }
+
+    public bool IsCanvasActive()
+    {
+        return _canvas.activeSeft;
+    }
+    
+}
---
+++ Gravilota/Assets/Scripts/Menu/MenuItemController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using 
+
+public class MenuItemController: MonoBehaviour
+{
+
+
+    const float _HOVERSCALEFACTOR = 1.4f;
+    MenuController _menuController;
+
+    private void Awake()
+    {
+        _menuController = GameObject.Find("Global")
+    }
+    public void OnMouseEnter()
+    {
+        transform.localScale *= _HOVERSCALEFACTOR;
+    }
+
+    public void OnMouseExit()
+    {
+        transform.localScale /=_HOVERSCALEFACTOR;
+    }
+
+    public void OnMouseUp()
+    {
+        if (_menuController.IsCanvasActive())
+        return;
+        switch (gameObject.name)
+        {
+            case "Jugar"
+            SceneManager.LoadScene("FlappyBird");
+            break;
+
+            case "Opciones"
+            _menuController.ShowGameOptions();
+            break;
+
+            case "Salir"
+            Application.Quit();
+            break;
+        }
+
+
+    }
+
+
+public void OkDialog()
+{
+    _menuController.HideGameOptions();
+}
+
+public void CancelDialog()
+{
+    _menuController.HideGameOptions();
+}
+
+public void OnPlayerNameChanged(InputField input)
+{
+    
+}
+
+    // Start is called before the first frame update
+}
